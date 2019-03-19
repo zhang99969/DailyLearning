b45369aae70bd0fbd9a3ee8c5db970cf4a899087
zhang99969 <1641899969@qq.com>
2019-03-19 19:35:51 +0800

    change codes

--- 21 _Software Testing Technology/备份/suplerlists/functional_tests2.py
+++ 21 _Software Testing Technology/备份/suplerlists/functional_tests2.py
@@ -10,8 +10,8 @@ import unittest
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser = webdriver.Chrome()
-    def tearDown(self):.
+        self.browser = webdriver.Firefox()
+    def tearDown(self):
         self.browser.quit()
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
