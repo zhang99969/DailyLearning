ad300e9fc22510d37a0dc32e172a897577093189
zhang99969 <1641899969@qq.com>
2019-03-20 10:09:16 +0800

    add funcional_test3 and change views urls codes

--- 21 _Software Testing Technology/suplerlists/lists/views.py
+++ 21 _Software Testing Technology/suplerlists/lists/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
-def home_page():
-    pass
+from django.http import HttpResponse
+def home_page(request):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
