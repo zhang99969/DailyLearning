7d75a9657ccdd9d48d3c329bde2a4973e02e52d1
zhang99969 <1641899969@qq.com>
2019-04-30 20:28:31 +0800

    add codes

--- 21 _Software Testing Technology/suplerlists/lists/views.py
+++ 21 _Software Testing Technology/suplerlists/lists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect,render
-from lists.models import Item
+from lists.models import Item, List
 def home_page(request):
     #return HttpResponse('<html><title>To-Do lists</title></html>')
   
@@ -32,9 +32,26 @@ def home_page(request):
 #        return redirect('/lists/the-only-list-in-the-world/')
     return render(request, 'home.html')
 
-def view_list(request):
-    items = Item.objects.all()
-    return render(request, 'list.html', {'items': items})
+def view_list(request,list_id):
+#    items = Item.objects.all()
+#    return render(request, 'list.html', {'items': items})
+    #
+#    list_ = List.objects.get(id=list_id)
+#    items = Item.objects.filter(list=list_)
+#    return render(request, 'list.html', {'items': items})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+#def new_list(request):
+#    Item.objects.create(text=request.POST['item_text'])
+#    return redirect('/lists/the-only-list-in-the-world/')
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list-in-the-world/')
+#    list_ = List.objects.create()
+#    Item.objects.create(text=request.POST['item_text'], list=list_)
+#    return redirect('/lists/the-only-list-in-the-world/')
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
