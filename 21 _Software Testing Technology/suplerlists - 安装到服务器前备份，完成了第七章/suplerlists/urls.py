3a34a3c4b8021d2f61717a6db1fe9b7fd9637712
zhang99969 <1641899969@qq.com>
2019-05-09 12:49:12 +0800

    add backup

---
+++ 21 _Software Testing Technology/suplerlists - 安装到服务器前备份，完成了第七章/suplerlists/urls.py
@@ -0,0 +1,44 @@
+"""suplerlists URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+#"""
+#from django.conf.urls import url
+#from lists import views
+#urlpatterns = [
+    #path('admin/', admin.site.urls),
+    #url(r'^$',views.home_page,name='home'),
+    #   
+    #url(r'^$', views.home_page, name='home'),
+    #url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    #
+#    url(r'^$', views.home_page, name='home'),
+#    url(r'^lists/new$', views.new_list, name='new_list'),
+#    url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list'),
+    #
+#    url(r'^$', views.home_page, name='home'),
+#    url(r'^lists/new$', views.new_list, name='new_list'),
+#    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+#    
+#    url(r'^$', views.home_page, name='home'),
+#    url(r'^lists/new$', views.new_list, name='new_list'),
+#    url(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+#    url(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+#]
+from django.conf.urls import include, url
+from lists import views as list_views
+from lists import urls as list_urls
+urlpatterns = [
+    url(r'^$', list_views.home_page, name='home'),
+    url(r'^lists/', include(list_urls)),
+]
